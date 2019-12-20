01d5e9ed9465ade1a252c1d7d500f938395b1a3a
artmadeit <this.arthur.email@gmail.com>
2019-12-20 01:17:07 -0500

    feat: remove addFile test button

--- src/gui-scripts/renderer.js
+++ src/gui-scripts/renderer.js
@@ -7,24 +7,19 @@ let progressBar = select("#progressBar");
 let version = select("#version");
 
 ipcRenderer.on("message", (event, text) => {
-    let message = document.createElement("div");
-    message.innerHTML = text;
-    container.appendChild(message);
+  let message = document.createElement("div");
+  message.innerHTML = text;
+  container.appendChild(message);
 });
 
 ipcRenderer.on("version", (event, text) => {
-    version.innerText = text;
+  version.innerText = text;
 });
 
 ipcRenderer.on("download-progress", (event, text) => {
-    progressBar.style.width = `${text}%`;
+  progressBar.style.width = `${text}%`;
 });
-const { getFiles, addTexToFile } = require("../modules/files");
-const { pentacamAutocsvPath, processFile } = require("../modules/watch");
-const path = require("path");
 
-select("#addFile").addEventListener("click", evt => {
-    addTexToFile(path.join(pentacamAutocsvPath, "INDEX-LOAD.CSV"), "\nHOLLAS");
-});
+const { pentacamAutocsvPath } = require("../modules/watch");
 
 document.getElementById("form-input").value = pentacamAutocsvPath;
