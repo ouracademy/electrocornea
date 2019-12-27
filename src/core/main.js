4ef836e02c6f4b1e15924b505b38d4da34bd2d38
artmadeit <this.arthur.email@gmail.com>
2019-12-27 01:14:46 -0500

    refactor: remove unused import

--- src/core/main.js
+++ src/core/main.js
@@ -1,74 +1,75 @@
 const { app, BrowserWindow } = require("electron");
 const { autoUpdater } = require("electron-updater");
-const path = require("path");
 
 let win;
 
 const dispatch = data => {
-    win.webContents.send("message", data);
+  win.webContents.send("message", data);
 };
 
 const createDefaultWindow = () => {
-    win = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    win.on("closed", () => {
-        win = null;
-    });
+  win.on("closed", () => {
+    win = null;
+  });
 
-    win.loadFile("src/gui/index.html");
-    // win.webContents.openDevTools();
+  win.loadFile("src/gui/index.html");
+  // win.webContents.openDevTools();
 
-    win.on("closed", function (event) {
-        watcher.close().then(() => (event.returnValue = false));
-    });
-    return win;
+  win.on("closed", function(event) {
+    watcher.close().then(() => (event.returnValue = false));
+  });
+  return win;
 };
 
 app.on("ready", () => {
-    createDefaultWindow();
+  createDefaultWindow();
 
-    autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify();
 
-    win.webContents.on("did-finish-load", () => {
-        win.webContents.send("version", app.getVersion());
-    });
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.send("version", app.getVersion());
+  });
 });
 
 app.on("window-all-closed", () => {
-    app.quit();
+  app.quit();
 });
 
 autoUpdater.on("checking-for-update", () => {
-    dispatch("Checking for update...");
+  dispatch("Checking for update...");
 });
 
 autoUpdater.on("update-available", info => {
-    dispatch("Update available. Download will start");
+  dispatch("Update available. Download will start");
 });
 
 autoUpdater.on("update-not-available", info => {
-    dispatch("Update not available.");
+  dispatch("Update not available.");
 });
 
 autoUpdater.on("error", err => {
-    dispatch("Error in auto-updater. " + err);
+  dispatch("Error in auto-updater. " + err);
 });
 
 autoUpdater.on("download-progress", progressObj => {
-    // let log_message = "Download speed: " + progressObj.bytesPerSecond
-    // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
-    // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
-    // dispatch(log_message)
-    dispatch("Update available. Download started");
-    win.webContents.send("download-progress", progressObj.percent);
+  // let log_message = "Download speed: " + progressObj.bytesPerSecond
+  // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  // dispatch(log_message)
+  dispatch("Update available. Download started");
+  win.webContents.send("download-progress", progressObj.percent);
 });
 
 autoUpdater.on("update-downloaded", info => {
-    dispatch("Update downloaded, please close and open this application to update it");
+  dispatch(
+    "Update downloaded, please close and open this application to update it"
+  );
 });
 
 const { startWatch } = require("../modules/watch");
