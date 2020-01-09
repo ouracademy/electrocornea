cfe4029190487fc63bb07b860e4467174fc72293
artmadeit <this.arthur.email@gmail.com>
2020-01-09 01:41:42 -0500

    fix: quickfix just file transport

--- src/modules/log.js
+++ src/modules/log.js
@@ -5,13 +5,19 @@ const { store } = require("./store");
 const loggingUri = store.get("loggingUriInput");
 
 const logger = winston.createLogger({
-  transports: loggingUri
-    ? [
-        new winston.transports.MongoDB({
-          db: loggingUri
-        })
-      ]
-    : [new winston.transports.Console()]
+  transports:
+    // loggingUri
+    // ? [
+    //     new winston.transports.MongoDB({
+    //       db: loggingUri
+    //     })
+    //   ]
+    // : [new winston.transports.Console()]
+    [
+      new winston.transports.File({
+        filename: "keratoconus-cornea.log"
+      })
+    ]
 });
 
 module.exports = {
