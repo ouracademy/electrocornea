3f187d54bb1e84dfdd4b2c872258bbccfc032d0e
qpdian <qpdiam@gmail.com>
2020-01-10 01:39:43 -0500

    feat: configure http transport

--- src/modules/log.js
+++ src/modules/log.js
@@ -5,22 +5,28 @@ const { store } = require("./store");
 const loggingUri = store.get("loggingUriInput");
 
 const logger = winston.createLogger({
-  transports:
-    // loggingUri
-    // ? [
-    //     new winston.transports.MongoDB({
-    //       db: loggingUri
-    //     })
-    //   ]
-    // : [new winston.transports.Console()]
-    [
-      new winston.transports.File({
-        filename: "keratoconus-cornea.log"
-      })
-    ]
+    transports:
+        // loggingUri
+        // ? [
+        //     new winston.transports.MongoDB({
+        //       db: loggingUri
+        //     })
+        //   ]
+        // : [new winston.transports.Console()]
+        [
+            // new winston.transports.File({
+            //     filename: "keratoconus-cornea.log"
+            // }),
+            new winston.transports.Http({
+                ssl: true,
+                host: "keratoconus-exams.herokuapp.com",
+                port: 443,
+                path: "winston"
+            })
+        ]
 });
 
 module.exports = {
-  logger,
-  loggingUri
+    logger,
+    loggingUri
 };
