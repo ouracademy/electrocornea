cac97266d47baa44b9f1a1424be6c7b90134acc6
qpdian <qpdiam@gmail.com>
2020-01-10 02:56:32 -0500

    feat: watch files NO-LOAD

--- src/modules/watch.js
+++ src/modules/watch.js
@@ -4,20 +4,33 @@ const path = require("path");
 /**
  * Used in Keratoconus REST API
  */
+const filesPentacam = [
+    "SUMMARY.CSV",
+    "PACHY.CSV",
+    "KEIO.CSV",
+    "INDEX.CSV",
+    "Fourier.CSV",
+    "COR-PWR.CSV",
+    "CorneoScleral.CSV",
+    "CHAMBER.CSV",
+    "BADisplay.CSV",
+    "AXLScan_Result.CSV"
+];
 const filesToAnalyze = [
-  "ZERNIKE-WFA.CSV",
-  "ZERNIKE-ELE.CSV",
-  "SUMMARY-LOAD.CSV",
-  "PACHY-LOAD.CSV",
-  "KEIO-LOAD.CSV",
-  "INDEX-LOAD.CSV",
-  "Fourier-LOAD.CSV",
-  "EccSag-LOAD.CSV",
-  "COR-PWR-LOAD.CSV",
-  "CorneoScleral-LOAD.CSV",
-  "CHAMBER-LOAD.CSV",
-  "BADisplay-LOAD.CSV",
-  "AXLScan_Result-LOAD.CSV"
+    "ZERNIKE-WFA.CSV",
+    "ZERNIKE-ELE.CSV",
+    "SUMMARY-LOAD.CSV",
+    "PACHY-LOAD.CSV",
+    "KEIO-LOAD.CSV",
+    "INDEX-LOAD.CSV",
+    "Fourier-LOAD.CSV",
+    "EccSag-LOAD.CSV",
+    "COR-PWR-LOAD.CSV",
+    "CorneoScleral-LOAD.CSV",
+    "CHAMBER-LOAD.CSV",
+    "BADisplay-LOAD.CSV",
+    "AXLScan_Result-LOAD.CSV",
+    ...filesPentacam
 ];
 
 const { store } = require("./store");
@@ -30,51 +43,54 @@ const isAnalyzable = aFile => filesToAnalyze.includes(aFile);
 const { getLastLine } = require("./files");
 
 const processFile = aFilePath => {
-  const aFileName = path.basename(aFilePath);
+    const aFileName = path.basename(aFilePath);
 
-  logger.info(`Leyendo..${aFileName}`);
-  if (!isAnalyzable(aFileName)) {
-    return;
-  }
+    logger.info(`Leyendo..${aFileName}`);
+    if (!isAnalyzable(aFileName)) {
+        return;
+    }
 
-  logger.info("Enviando request...");
-  getLastLine(aFilePath)
-    .then(sendRequest(aFileName))
-    .catch(err => logger.warn(err));
+    logger.info("Enviando request...");
+    getLastLine(aFilePath)
+        .then(sendRequest(aFileName))
+        .catch(err => logger.warn(err));
 };
 
 const url = "https://keratoconus-exams.herokuapp.com/exams-file";
 const axios = require("axios").default;
 
 const sendRequest = aFileName => lastLine => {
-  axios
-    .post(url, { file: aFileName, data: lastLine })
-    .then(response =>
-      logger.info(
-        "Success:" + response.status + ", data:" + JSON.stringify(response.data)
-      )
-    )
-    .catch(error => logger.warn(JSON.stringify(error)));
+    axios
+        .post(url, { file: aFileName, data: lastLine })
+        .then(response =>
+            logger.info(
+                "Success:" +
+                    response.status +
+                    ", data:" +
+                    JSON.stringify(response.data)
+            )
+        )
+        .catch(error => logger.warn(JSON.stringify(error)));
 };
 
 const startWatch = () => {
-  const watcher = chokidar.watch(
-    filesToAnalyze.map(x => path.join(pentacamAutocsvPath, x)),
-    {
-      awaitWriteFinish: true
-    }
-  );
-  watcher.on("change", path => {
-    processFile(path);
-  });
-  return watcher;
+    const watcher = chokidar.watch(
+        filesToAnalyze.map(x => path.join(pentacamAutocsvPath, x)),
+        {
+            awaitWriteFinish: true
+        }
+    );
+    watcher.on("change", path => {
+        processFile(path);
+    });
+    return watcher;
 };
 
 module.exports = {
-  pentacamAutocsvPath,
-  filesToAnalyze,
-  startWatch,
-  isAnalyzable,
-  processFile,
-  store
+    pentacamAutocsvPath,
+    filesToAnalyze,
+    startWatch,
+    isAnalyzable,
+    processFile,
+    store
 };
