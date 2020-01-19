17860cc4210f344f78273c9583b0cc1d78e1f438
artmadeit <this.arthur.email@gmail.com>
2020-01-19 18:24:31 -0500

    feat: timestamp in winston

--- src/modules/log.js
+++ src/modules/log.js
@@ -1,32 +1,34 @@
-const winston = require("winston");
+const { createLogger, format, transports } = require("winston");
+
 require("winston-mongodb");
 
 const { store } = require("./store");
 const loggingUri = store.get("loggingUriInput");
 
-const logger = winston.createLogger({
-    transports:
-        // loggingUri
-        // ? [
-        //     new winston.transports.MongoDB({
-        //       db: loggingUri
-        //     })
-        //   ]
-        // : [new winston.transports.Console()]
-        [
-            new winston.transports.File({
-                filename: "keratoconus-cornea.log"
-            }),
-            new winston.transports.Http({
-                ssl: true,
-                host: "keratoconus-exams.herokuapp.com",
-                port: 443,
-                path: "winston"
-            })
-        ]
+const logger = createLogger({
+  format: format.combine(format.timestamp(), format.json()),
+  transports:
+    // loggingUri
+    // ? [
+    //     new transports.MongoDB({
+    //       db: loggingUri
+    //     })
+    //   ]
+    // : [new transports.Console()]
+    [
+      new transports.File({
+        filename: "keratoconus-cornea.log"
+      })
+      // new transports.Http({
+      //   ssl: true,
+      //   host: "keratoconus-exams.herokuapp.com",
+      //   port: 443,
+      //   path: "winston"
+      // })
+    ]
 });
 
 module.exports = {
-    logger,
-    loggingUri
+  logger,
+  loggingUri
 };
