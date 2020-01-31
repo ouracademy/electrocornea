3021cde65d8fecdb205f99cce1e7f6c7c8f5f303
artmadeit <this.arthur.email@gmail.com>
2020-01-31 15:22:39 -0500

    chore: replace axios by got :'(

--- src/modules/send-request.js
+++ src/modules/send-request.js
@@ -1,14 +1,20 @@
 const { logger } = require("./log");
+const got = require("got");
 
-const url = "https://keratoconus-exams.herokuapp.com/exams-file";
-const axios = require("axios").default;
+const url = "http://127.0.0.1:8000/exams-file"; // "https://keratoconus-exams.herokuapp.com/exams-file";
 
 const sendRequest = aFileName => lastLine =>
-  axios
-    .post(url, { file: aFileName, data: lastLine })
+  got
+    .post(url, {
+      json: { file: aFileName, data: lastLine },
+      responseType: "json"
+    })
     .then(response =>
       logger.info(
-        "Success:" + response.status + ", data:" + JSON.stringify(response.data)
+        "Success:" +
+          response.statusCode +
+          ", data:" +
+          JSON.stringify(response.body)
       )
     )
     .catch(error => logger.warn(JSON.stringify(error)));
